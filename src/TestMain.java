c2ece24f88aa9a361ac4e0a5a9b373836dbe589c
SunShuai159 <sun400070@163.com>
2020-06-20 08:33:14 +0800

    1

---
+++ src/TestMain.java
@@ -0,0 +1,9 @@
+public class TestMain {
+    public static void main(String[] args) {
+        System.out.println("哈哈哈哈哈哈哈");
+        System.out.println("哈哈哈哈哈哈哈哈哈哈哈哈哈");
+        System.out.println("哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈");
+        System.out.println("哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈hhhhhhhhhhhh");
+        System.out.println("哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈hhhhhhhhhhhhhhhhhhhhhhhhhh");
+    }
+}
